ec49d3ceeff96736d4ccaaa18659c166942e2b15
sonhoang.epu <sonhoang.epu@gmail.com>
2023-01-28 22:21:10 +0700

    fix detail

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,29 +1,37 @@
 import React from "react";
 import styles from "./card.module.scss";
-import Link from "next/link";
+import { useDispatch } from "react-redux";
+import { useRouter } from "next/router";
+import { setProductDetail } from "../../stores/productsSlice";
 
 export interface CardProps {
   image: string;
   name: string;
   price: number;
-  id: string;
+  id: number;
   slug: string;
   localize?: string;
   product_name?: string;
 }
 
 export const Card = ({ data }: { data: CardProps }) => {
+  const router = useRouter();
+  const dispatch = useDispatch();
+  const onChange = () => {
+    router.push(`/products/${data.slug}`);
+    dispatch(setProductDetail(data));
+  };
   return (
     <div className={styles.card}>
       <div className={styles.thumbnail}>
-        <Link href={`/products/${data.slug}`}>
+        <div onClick={onChange}>
           <img className={styles.image} src={data.image} alt={""} />
-        </Link>
+        </div>
       </div>
       <div className={styles.content}>
-        <Link href={`/products/${data.slug}`}>
+        <div onClick={onChange}>
           <h3 className={styles.name}>{data.product_name || data.name}</h3>
-        </Link>
+        </div>
         <span className={styles.price}>
           {data.price.toLocaleString("vi", {
             style: "currency",
--- src/pages/products/[slug]/index.tsx
+++ src/pages/products/[slug]/index.tsx
@@ -1,16 +1,29 @@
 import { useCallback, useEffect, useState } from "react";
 import { getProductById } from "../../../services/products";
 import styles from "../products.module.scss";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { RootState } from "../../../stores";
 import Link from "next/link";
 
 const Product = () => {
-  const [productDetail, setProduct] = useState<any>({});
+  const [productDetail, setProduct] = useState<any>(
+    useSelector((state: RootState) => state.products.product)
+  );
   const [quantity, setQuantity] = useState<number>(1);
+  const [total, setTotal] = useState<number>(0);
+  const [size, setSize] = useState<boolean>(false);
+  const [option, setOption] = useState<boolean>(false);
 
   const onChangeQuantity = (type: "plus" | "minus") => {
     setQuantity(type === "plus" ? quantity + 1 : quantity - 1);
+  };
+
+  const onChecked = (type: "size" | "option", value: boolean) => {
+    if (type === "size") {
+      setSize(value);
+    } else {
+      setOption(value);
+    }
   };
 
   const fetchSituation = useCallback(async () => {
@@ -23,9 +36,18 @@ const Product = () => {
   }, []);
 
   useEffect(() => {
-    fetchSituation().then();
+    if (!productDetail) fetchSituation().then();
     //eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  useEffect(() => {
+    let newTotal = productDetail.price;
+    if (size) newTotal = newTotal + 6000;
+    if (option) newTotal = newTotal + 10000;
+
+    setTotal(newTotal * quantity);
+  }, [productDetail, quantity, size, option]);
+
   if (!productDetail) return null;
 
   return (
@@ -72,8 +94,102 @@ const Product = () => {
             </div>
             <div className={styles.boxOptions}>
               <div className={styles.boxOptionHeader}>CHỌN SIZE (BẮT BUỘC)</div>
-              <div className={styles.boxOptionContent}></div>
+              <div className={styles.boxOptionContent}>
+                <div className={styles.radioOption}>
+                  <label className={styles.radioCustom}>
+                    size nho +0
+                    <input
+                      type="radio"
+                      name="size"
+                      defaultChecked
+                      onChange={() => onChecked("size", false)}
+                    />
+                    <span className={styles.checkmark}></span>
+                  </label>
+                </div>
+                <div className={styles.radioOption}>
+                  <label className={styles.radioCustom}>
+                    size vua +6.000
+                    <input
+                      type="radio"
+                      name="size"
+                      onChange={() => onChecked("size", true)}
+                    />
+                    <span className={styles.checkmark}></span>
+                  </label>
+                </div>
+              </div>
+              <div className={styles.boxOptionHeader}>
+                CHỌN TOPPING (CHỌN 1)
+              </div>
+              <div className={styles.boxOptionContent}>
+                <div className={styles.radioOption}>
+                  <label className={styles.radioCustom}>
+                    None + 0
+                    <input
+                      type="radio"
+                      name="topping"
+                      defaultChecked
+                      onChange={() => onChecked("option", false)}
+                    />
+                    <span className={styles.checkmark}></span>
+                  </label>
+                </div>
+                <div className={styles.radioOption}>
+                  <label className={styles.radioCustom}>
+                    Kem Phô Mai Macchiato + 10.000
+                    <input
+                      type="radio"
+                      name="topping"
+                      onChange={() => onChecked("option", true)}
+                    />
+                    <span className={styles.checkmark}></span>
+                  </label>
+                </div>
+                <div className={styles.radioOption}>
+                  <label className={styles.radioCustom}>
+                    Trân châu trắng + 10.000
+                    <input
+                      type="radio"
+                      name="topping"
+                      onChange={() => onChecked("option", true)}
+                    />
+                    <span className={styles.checkmark}></span>
+                  </label>
+                </div>
+                <div className={styles.radioOption}>
+                  <label className={styles.radioCustom}>
+                    Caramel + 10.000
+                    <input
+                      type="radio"
+                      name="topping"
+                      onChange={() => onChecked("option", true)}
+                    />
+                    <span className={styles.checkmark}></span>
+                  </label>
+                </div>
+                <div className={styles.radioOption}>
+                  <label className={styles.radioCustom}>
+                    Thạch Cà Phê + 10.000
+                    <input
+                      type="radio"
+                      name="topping"
+                      onChange={() => onChecked("option", true)}
+                    />
+                    <span className={styles.checkmark}></span>
+                  </label>
+                </div>
+              </div>
             </div>
+            <button className={styles.orderButton}>
+              <span>
+                {total.toLocaleString("vi", {
+                  style: "currency",
+                  currency: "VND",
+                })}
+              </span>{" "}
+              - Thêm vào giỏ hàng
+            </button>
           </div>
         </div>
       </div>
